0910d6f3ec22cd79e0a4c705a10224f15cb0b387
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-04 16:39:52 +0200

    :wrench: (eslint) update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/strongly-recommended"
-  ],
-  // add your custom rules here
+  extends: ["plugin:vue/strongly-recommended"],
   rules: {
     "no-console": "off",
     "no-debugger": "off",
-    "vue/no-side-effects-in-computed-properties": "off",
-    "vue/no-async-in-computed-properties": "off"
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: "babel-eslint",
+  },
 }
